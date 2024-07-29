7357ee9699be5565dd9bb664f0f86747d46e1838
0Jsnipes <jared.c.snipes@gmail.com>
2024-07-29 14:57:59 -0400

    done

--- frontend/components/tests/App.test.js
+++ frontend/components/tests/App.test.js
@@ -1,6 +1,6 @@
-// 👇 YOUR WORK STARTS ON LINE 19
+YOUR WORK STARTS ON LINE 19
 import React from 'react'
-import { render, waitFor, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import server from '../../../backend/mock-server'
@@ -16,11 +16,21 @@ describe('Stranger Things App', () => {
     user = userEvent.setup()
   })
   test('App mounts without crashing', () => {
+    screen.debug()
     // 👉 TASK: print the simulated DOM using screen.debug
   })
   test('App renders the correct texts', async () => {
     // 👉 TASK: click on the button that displays "Press to Get Show Data"
 
+await user.click(screen.getByText('Press to Get Show Data'))
+
+await (() => {
+  expect(screen.queryByText('Press to Get Show Data')).not.toBeInTheDocument()
+  screen.getByText("Stranger Things")
+  screen.getByText("A love letter to the '80s classics \
+    that captivated a generation", { exact: false })
+  screen.getByText("Select A Season")
+})
     // 👉 TASK: create a waitFor and await for the following to be true:
     //    - The text "Press to Get Show Data" is no longer in the DOM
     //    - The text "Stranger Things" exists in the DOM
@@ -32,12 +42,14 @@ describe('Stranger Things App', () => {
     // ❗ Don't forget user actions need the await keyword
     // ❗ Use the selectOptions user action
     // ❗ Grab the select element using querySelector
-
+await user.selectOptions(document.querySelector('select'), '1')
     // 👉 TASK: create the following assertions:
     //    - The text "Season 2, Episode 1" exists in the DOM
     //    - The text "Chapter One: MADMAX" exists in the DOM
     //    - The text "One year after the events with the Upside Down and the Demogorgon" exists in the DOM
     // ❗ You will need { exact: false } to select the longer text
-
+screen.getByText("Season 2, Episode 1")
+screen.getByText("Chapter One: MADMAX")
+screen.getByText("One year after the events with the Upside Down and the Demogorgon", {exact: false})
   })
 })
